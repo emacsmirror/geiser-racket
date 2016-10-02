1facdcd1ba1ebc4b4352edbdea034389fa2c3d90
Jose Antonio Ortega Ruiz <jao@gnu.org>
2016-10-02 03:54:29 +0200

    racket: indentation for 'shared'

--- elisp/geiser-racket.el
+++ elisp/geiser-racket.el
@@ -1,6 +1,6 @@
 ;; geiser-racket.el -- geiser support for Racket scheme
 
-;; Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015 Jose Antonio Ortega Ruiz
+;; Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 Jose Antonio Ortega Ruiz
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the Modified BSD License. You should
@@ -348,6 +348,7 @@ using start-geiser, a procedure in the geiser/server module."
  (splicing-letrec-syntaxes+values 1)
  (splicing-letrec-values 1)
  (splicing-local 1)
+ (shared 1)
  (struct 1)
  (syntax-id-rules defun)
  (syntax/loc 1)
